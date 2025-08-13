d8657b24c7e7af5b124a69e1a81151765fa3a894
Nwaiwuchi10 <djnchrys@gmail.com>
2025-08-13 13:33:32 -0700

    new

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ import * as bodyParser from 'body-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import * as path from 'path';
-import { existsSync, mkdirSync } from 'fs';
+// import { existsSync, mkdirSync } from 'fs';
 
 import * as express from 'express';
 
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
-  const uploadsDir = join(process.cwd(), 'FileUploads');
-  if (!existsSync(uploadsDir)) {
-    mkdirSync(uploadsDir);
-  }
+  // const uploadsDir = join(process.cwd(), 'FileUploads');
+  // if (!existsSync(uploadsDir)) {
+  //   mkdirSync(uploadsDir);
+  // }
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
@@ -25,9 +25,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  app.useStaticAssets(join(process.cwd(), 'FileUploads'), {
-    prefix: '/uploads/',
-  });
+  // app.useStaticAssets(join(process.cwd(), 'FileUploads'), {
+  //   prefix: '/uploads/',
+  // });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.enableCors();
 
